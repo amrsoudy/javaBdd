3b23d740156f8ab1f77b4fc9c889faf44d7a4873
amrsoudy <32444508+amrsoudy@users.noreply.github.com>
2018-05-28 11:57:28 -0400

    upload

--- BDDAvecCretiria/src/aptctr/Main.java
+++ BDDAvecCretiria/src/aptctr/Main.java
@@ -1,5 +1,9 @@
 package aptctr;
 
+import java.util.List;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 import persistance.Customer;
 
 /*
@@ -7,7 +11,6 @@ import persistance.Customer;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author 1795162
@@ -18,16 +21,27 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        Customer customer = new Customer();
-        
-        util.insert(customer);
-        
-        util.update(customer);
-        
-        util.delete(customer);
-        
-        
+
+//        Customer customer = new Customer();
+//
+//        util.insert(customer);
+//         
+//
+//        util.update(customer);
+//        String query = null;
+//        String x = "from Customer where customerId = :x";
+//        util.selectQuery(x,36);
+//        util.selectQuery(x,106);
+        String x2 = "select p.orderNum,p.quantity,p.shippingCost,p.shippingDate,p.customerId,c.name,count(*) from PurchaseOrder p ,Customer c where p.customerId=c.customerId "
+                + " group by p.orderNum,p.quantity,p.shippingCost,p.shippingDate,p.customerId,c.name";
+        util.selectPoWithCustomer(x2);
+
+
+        //util.listCustomer_idCGT(3);
+        //util.listCustomerNamesStartWith("b");
+        // util.ListPOForCustomer_id(36);
+//
+//        util.delete(customer);
     }
-    
+
 }
